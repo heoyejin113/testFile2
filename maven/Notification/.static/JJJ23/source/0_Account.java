be8c4080d2669405dd5c66db48bb32b1be6c011f
heoyejin113 <hyj5829@gmail.com>
2021-06-01 12:02:40 +0900

    q

---
+++ maven/Notification/.static/JJJ23/source/0_Account.java
@@ -0,0 +1,13 @@
+package com.codescroll.notification.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Account {//commitTest입니다. 
+  private long id;
+  private boolean active;
+  private String email;
+  private String username;
+}
---
+++ maven/Notification/.static/JJJ23/source/7_MAIL_SERVER_PROTOCOL.java
@@ -0,0 +1,6 @@
+package com.codescroll.notification.constants;
+
+public enum MAIL_SERVER_PROTOCOL {
+  smtp,
+  secure_smtp
+}
