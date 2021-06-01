586d7ab1833d7c24a8ac629d4c34232b47c72cf2
heoyejin113 <hyj5829@gmail.com>
2021-06-01 14:09:28 +0900

    11

--- 2010_CPP_x86_cube/cubeview.h
+++ 2010_CPP_x86_cube/cubeview.h
@@ -32,7 +32,7 @@ public:
 	BOOL bSetupPixelFormat(void);
 	unsigned char ComponentFromIndex(int i, UINT nbits, UINT shift);
 	void DrawScene(void);
-	printf("hello!");
+	 printf("Hello %s.\n", "world!");
 
 // Overrides
 	// ClassWizard generated virtual function overrides
